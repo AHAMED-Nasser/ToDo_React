72a88f48ff906819df46a8387e338ad0b526e484
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-03-26 23:59:08 +0100

    feature : affichage des tâche avec le JSON

---
+++ app/src/components/TaskList.js
@@ -0,0 +1,27 @@
+import '../css/TaskList.css';
+export function TaskList({tasks}) {
+    return (
+        <ul>
+            {tasks.map(task => (
+                <li key={task.id}>
+                    <strong className={'title-task'}>{task.title}</strong>
+                    <p>Description : {task.description}</p>
+                    <p>Date création : {task.date_creation}</p>
+                    <p>Date échéance : {task.date_echeance}</p>
+                    <p>Etat : {task.etat}</p>
+                    <strong>Equipiers</strong>
+                    {task.equipiers && task.equipiers.length > 0 ? (
+                        <ul className={'team-list'}>
+                            {task.equipiers.map((team) => (
+                                <li key={team.name}>{team.name}</li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p><i>Aucun équipier</i></p>
+                    )}
+
+                </li>
+            ))}
+        </ul>
+    )
+}
