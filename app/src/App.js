7d8dae2b68a09e2eee8f55e774dc8912e642b0a1
AHAMED-Nasser <137123083+AHAMED-Nasser@users.noreply.github.com>
2026-03-27 00:54:54 +0100

    Dev (#1)
    * feature : affichage des tâche avec le JSON
    
    * maj : rangement en grille des noms d'équipes
    
    ---------
    
    Co-authored-by: nularyNasser <137123083+nularyNasser@users.noreply.github.com>

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {TaskList} from "./components/TaskList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('/data.json')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            <h1 className={'home-title'}>TODO List</h1>
+            </header>
+            <main>
+                <TaskList tasks={data.taches} />
+            </main>
+        </div>
+    );
 }
 
 export default App;
