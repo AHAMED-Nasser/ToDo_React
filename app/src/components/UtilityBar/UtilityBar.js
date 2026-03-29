7009c45efd3263b4e7e62f20f68d6416297ebda5
nularyNasser <137123083+nularyNasser@users.noreply.github.com>
2026-03-29 21:30:22 +0200

    réglage du style

--- app/src/components/UtilityBar/UtilityBar.js
+++ app/src/components/UtilityBar/UtilityBar.js
@@ -3,7 +3,7 @@ import './UtilityBar.css';
 import {AddTask} from "../AddTask/AddTask";
 import {SelectMode} from "../SelectMode/SelectMode";
 import {TaskFilter} from "../TaskFilter/TaskFilter";
-import {useState} from "react";
+import React, {useState} from "react";
 
 export function UtilityBar({onSearch, currentMode, onModeChange, onAddTask}) {
     const handleSearch = (value) => {
